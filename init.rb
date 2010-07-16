fe544e6934323946878b80b2368cb0dfe8f22f04
Rico Sta. Cruz <rico@ambiescent.com>
2010-07-16 23:12:07 +0800

    Use hashListen.

--- init.rb
+++ init.rb
@@ -32,6 +32,22 @@ class Main < Sinatra::Base
       c = CodeRay.scan(code, language.to_sym).span(:css => :class)
       c.gsub("\n", "<br>")
     end
+
+    def js_assets(package)
+      files = Array.new
+      if settings.development?
+        files << '/js/jquery.min.js'
+      else
+        files << 'http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'
+      end
+
+      files << '/js/jquery.hashlisten.js'
+      files << '/js/script.js'
+
+      files.map do |file|
+        "<script type='text/javascript' src='#{file}'></script>"
+      end.join("\n")
+    end
   end
 end
 
--- public/js/script.js
+++ public/js/script.js
@@ -3,34 +3,57 @@
 
   $.extend(RF, {
     init: function() {
+      var self = this;
+
+      $.hashListen('', function() {
+        self.loadLanguages(['ruby', 'python']);
+        $(".preload").show();
+      });
+
+      $.hashListen(':languages', function(languages) {
+        languages = languages.split('+');
+        self.loadLanguages(languages);
+        $(".preload").show();
+      });
+    },
+
+    // Loads the languages given in the array.
+    loadLanguages: function(languages) {
+      var self = this;
+      for (i in self.languages) {
+        var lang = self.languages[i];
+        var $el  = $('.' + lang);
+
+        if (languages.indexOf(lang) != -1) {
+          $('.' + lang + ':hidden').show();
+          self.$checkbox(lang).attr('checked', 1);
+        }
+        else {
+          $('.' + lang).hide();
+          self.$checkbox(lang).attr('checked', 0);
+        }
+      }
+    },
+
+    // Returns the selected languages.
+    getLanguages: function() {
+      var $selector = $('.selector');
+      var re = [];
+      $selector.find('input:checkbox').each(function() {
+        if ($(this).attr('checked'))
+          { re.push($(this).attr('name')); }
+      });
+      return re;
+    },
+
+    $checkbox: function(lang) {
+      return $('.selector input:checkbox[name='+lang+']');
     }
   });
 
   $('.selector input:checkbox').live('change', function() {
-    var v    = $(this).attr('checked'),
-        name = $(this).attr('name');
-
-    if (v) {
-      $("." + name).fadeIn('fast');
-    }
-    else {
-      $("." + name).fadeOut('fast');
-    }
+    window.location.hash = '#' + RF.getLanguages().join('+');
   });
 
-  $(function() {
-    $(".selector").find("input:checkbox[name=ruby]").attr("checked", 1);
-    $(".selector").find("input:checkbox[name=python]").attr("checked", 1);
-    for (i in RF.languages) {
-      var lang      = RF.languages[i];
-      var checkbox  = $(".selector input:checkbox[name="+lang+"]");
-      var $elements = $("." + lang);
-
-      if (checkbox.attr("checked"))
-        { $elements.show(); }
-      else
-        { $elements.hide(); }
-    }
-    $(".preload").show();
-  });
+  $(function() { RF.init(); });
 })(jQuery);
